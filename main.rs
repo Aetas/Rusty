99ada95eaf730d752a92c8a2b4a5285593be7bd5
Derek Prince <ace.prince08@gmail.com>
2015-05-01 13:04:46 -0600

    Trying out comments. Nothing new.

--- main.rs
+++ main.rs
@@ -1,4 +1,8 @@
 
+//Obligatory Hello World.
+//Computer peeps have a strange love for this program.
+//Come to think of it, this is probably the only program that has been written in every single language ever made. 
+//*That can print words.
 fn main() {
   println!("Hello, world!");
 }
