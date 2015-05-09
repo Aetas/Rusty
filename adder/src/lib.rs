7f1f408af95f880267a59db5d3ff2e4b37a4fabd
Derek Prince <ace.prince08@gmail.com>
2015-05-09 16:53:50 -0600

    Added first test function. Doesn't really do much, but its cool. Seems to be a theme with rust.

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,4 +1,9 @@
 #[test]
+#[should_panic(expected = "assertion failed")]  //tells it to only give a bye/bi/by? if matched error
 fn it_works() {
+    assert_eq!("Hello", "World");
 }
+
+//assert(argument/evaluation) - sets panic! if passed false
+//assert_eq(arg1, arg2) - compares two arguments and sets panic if they do not match
 
