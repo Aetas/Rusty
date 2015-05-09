695efa87197a32b53c2647b44ab2b090afd44911
Derek Prince <ace.prince08@gmail.com>
2015-05-09 16:56:44 -0600

    Cool test implementation to check functionality.

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,7 +1,11 @@
+pub fn add_two(a:i32) -> i32 {
+    a + 2
+}
+
 #[test]
-#[should_panic(expected = "assertion failed")]  //tells it to only give a bye/bi/by? if matched error
+//#[should_panic(expected = "assertion failed")]  //tells it to only give a bye/bi/by? if matched error
 fn it_works() {
-    assert_eq!("Hello", "World");
+    assert_eq!(4, add_two(2));
 }
 
 //assert(argument/evaluation) - sets panic! if passed false
