9a07440b549635e4b80ae1911b916144fae87d29
Derek Prince <ace.prince08@gmail.com>
2015-05-14 16:05:45 -0600

    Added some maybe-working code to try target build cfg

---
+++ conditional/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    if cfg!(target_os = "macos) || cfg!(target_os = "ios") {
+        println!("Think Different!");
+    }
+    else
+        println!("That's also great.");
+
+}
