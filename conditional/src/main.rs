2828206f40de0eecd4645e940edbb9dd114e6f54
Derek Prince <ace.prince08@gmail.com>
2015-05-14 16:14:24 -0600

    Missed a double quote terminator. Whoops.

--- conditional/src/main.rs
+++ conditional/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    if cfg!(target_os = "macos) || cfg!(target_os = "ios") {
+    if cfg!(target_os = "macos") || cfg!(target_os = "ios") {
         println!("Think Different!");
     }
     else
-        println!("That is also great.");
-
+        println!("That's also great.");
 }
