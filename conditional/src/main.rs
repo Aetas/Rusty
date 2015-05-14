040075e9051e9afd495b857ef196fc143aad92a7
Derek Prince <ace.prince08@gmail.com>
2015-05-14 16:17:19 -0600

    as it turns out, double braces are not implied in rust, but required. Even for 1-liners.

--- conditional/src/main.rs
+++ conditional/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     if cfg!(target_os = "macos") || cfg!(target_os = "ios") {
         println!("Think Different!");
     }
-    else
+    else {
         println!("That's also great.");
+    }
 }
