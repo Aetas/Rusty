f359b76fab85985ecf6548cfd1e4bc1df5c7b631
Derek Prince <ace.prince08@gmail.com>
2017-06-11 13:03:57 -0600

    Added base for week 1 of embedded

---
+++ EmbeddedSummerDays/Warmup/target/debug/Warmup.d
@@ -0,0 +1,1 @@
+C:\Users\Aetas\Documents\GitProjects\Rusty\EmbeddedSummerDays\Warmup\target\debug\Warmup.exe: C:\Users\Aetas\Documents\GitProjects\Rusty\EmbeddedSummerDays\Warmup\src\main.rs
---
+++ philosopher_concurrency/target/debug/philosopher_concurrency.d
@@ -0,0 +1,1 @@
+C:\Users\Aetas\Documents\GitProjects\Rusty\philosopher_concurrency\target\debug\philosopher_concurrency.exe: C:\Users\Aetas\Documents\GitProjects\Rusty\philosopher_concurrency\src\main.rs
